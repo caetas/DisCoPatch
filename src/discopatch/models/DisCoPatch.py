d87c6a3ba0f72f55bff7fd24304a2a18bdba9405
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-05-13 16:33:21 +0200

    training bug

--- src/discopatch/models/DisCoPatch.py
+++ src/discopatch/models/DisCoPatch.py
@@ -210,8 +210,8 @@ class Discriminator(nn.Module):
             modules.append(
                 nn.Sequential(
                     nn.Conv2d(input_channels, h_dim, kernel_size = 3, stride = 2, padding = 1),
-                    #nn.BatchNorm2d(h_dim, track_running_stats=False),
-                    nn.GroupNorm(32, h_dim),
+                    nn.BatchNorm2d(h_dim, track_running_stats=False),
+                    #nn.GroupNorm(32, h_dim),
                     #nn.InstanceNorm2d(h_dim),
                     nn.LeakyReLU()
                 )
